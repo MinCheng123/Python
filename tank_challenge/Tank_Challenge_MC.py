406d88b76f9749e08b8399eadb35a8e7c718fd4b
unknown <minc@student.chalemrs.se>
2019-08-31 23:01:17 +0200

    nothing

--- tank_challenge/Tank_Challenge_MC.py
+++ tank_challenge/Tank_Challenge_MC.py
@@ -43,62 +43,73 @@ class Coordination:
         self.old_coordinate=[-1,-1]
         self.old_value=0
         self.lidar_distance_front=api.lidar_front()
-        self.object_coordinate=[9,9] #[row][colomn] lvl1
-        
+        self.object_coordinate=[11,10] #[row][colomn] lvl1
+        self.old_direction=0
         
     def tank_track(self):
-        self.old_coordinate=self.tank_coordinate.copy()
-        self.old_value=self.matrix[self.tank_coordinate[0]][self.tank_coordinate[1]]
+        
         self.direction_detection_tank()  # tank_coordiante update 
 
-        print('old_coordinate',self.old_coordinate)
+#        print('old_coordinate',self.old_coordinate)
+        print(self.old_value)
+        
+        self.matrix[self.old_coordinate[0]][self.old_coordinate[1]]=0
         self.matrix[self.tank_coordinate[0]][self.tank_coordinate[1]]=9 
-        self.matrix[self.old_coordinate[0]][self.old_coordinate[1]]=0  #### self.old_value
         print( 'tank coordinate:',self.tank_coordinate)
 #        print('old_value',self.old_value)
+        self.old_direction = self.Direction.copy()
         self.print_map()
-        
+        self.old_coordinate=self.tank_coordinate.copy()
+        self.old_value=self.matrix[self.old_coordinate[0]][self.old_coordinate[1]]
             
     def object_detect(self,direction):
-         if self.lidar_distance_front and api.identify_target() is False:
-            self.direction_detection_object( self.lidar_distance_front )
-            print(self.object_coordinate)
+        self.lidar_distance_front=api.lidar_front()
+        if self.lidar_distance_front and api.identify_target() is False:
+            self.direction_detection_object(self.lidar_distance_front  )
+            print('obstacle distance',self.object_coordinate,'lidar_front',self.lidar_distance_front)
             self.matrix[ self.object_coordinate[0] ][ self.object_coordinate[1] ] = 1 
-            self.print_map()
+#            self.print_map()
             
              
     def direction_detection_tank(self):
-
-        for index, element in enumerate(self.Direction):
-            if element == 1:
-                if index == 0: #up
-                    self.tank_coordinate[0]-=1 
-                elif index ==1: #left
-                    self.tank_coordinate[1]-=1
-                elif index ==2: #down  
-                    self.tank_coordinate[0]+=1 
-                elif index ==3: #right 
-                    self.tank_coordinate[1]+=1 
-                print(self.tank_coordinate)  
+        print('tank direction',self.Direction,'same direction', self.old_direction == self.Direction)
+        
+        if self.old_direction == self.Direction:
+            for index, element in enumerate(self.Direction):
+                if element == 1:
+                    if index == 0: #up
+                        self.tank_coordinate[0]-=1 
+                    elif index ==1: #left
+                        self.tank_coordinate[1]-=1
+                    elif index ==2: #down  
+                        self.tank_coordinate[0]+=1 
+                    elif index ==3: #right 
+                        self.tank_coordinate[1]+=1 
+#                print(self.tank_coordinate)  
     
     def direction_detection_object(self,distance):
         
-        #print(self.Direction)
+        print('object direction',self.Direction)
         for index, element in enumerate(self.Direction):
             if element == 1:
                 if index == 0: #up
-                    self.object_coordinate[0]-=distance 
+                    self.object_coordinate[0]=self.tank_coordinate[0]-distance 
+                    self.object_coordinate[1]=self.tank_coordinate[1]
                 elif index ==1: #left
-                    self.object_coordinate[1]-=distance 
+                    self.object_coordinate[1]=self.tank_coordinate[1]-distance
+                    self.object_coordinate[0]=self.tank_coordinate[0]
                 elif index ==2: #down  
-                    self.object_coordinate[0]+=distance 
+                    self.object_coordinate[0]=self.tank_coordinate[0]+distance
+                    self.object_coordinate[1]=self.tank_coordinate[1]
                 elif index ==3: #right 
-                    self.object_coordinate[1]+=distance  
-
+                    self.object_coordinate[1]=self.tank_coordinate[1]+distance  
+                    self.object_coordinate[0]=self.tank_coordinate[0]    
         
     def print_map(self):
         for row in self.matrix:
-            print(row)          
+            print(row)    
+
+    
         
 class Solution:
     
@@ -106,6 +117,7 @@ class Solution:
         # If you need initialization code, you can write it here!
         # Do not remove.
         self.direction = [1,0,0,0] # up left down right
+#        self.old_direction= [-1,-1,-1,-1]
         self.Direction = Direction(self.direction)
         self.Coordination=Coordination(self.direction)
         self.target = True
@@ -121,10 +133,16 @@ class Solution:
         """
         # Todo: Write your code here!
         #self.Coordination.object_detect(self.direction)
+        
         self.Coordination.tank_track()
         if api.lidar_front() <= 1:
             
             self.Direction.go_right()
-            self.target=api.identify_target()
-            
+#            self.target=api.identify_target()
+#        print('same.direction',self.direction==self.old_direction)
+       
+        
+        
+        self.Coordination.object_detect(self.direction)
         api.move_forward()
+        
